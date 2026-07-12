12f4063f7c8078110709cc45c888dad70ef0b1dd
Chris Anders <filmee24@gmail.com>
2026-07-12 18:39:59 +0200

    Init

---
+++ src/RomManager/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace RomManager.Views;
+
+/// <summary>Main window view.</summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
