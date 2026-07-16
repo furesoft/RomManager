4f47450b3d712cb50fc9d77ad5722e8395068a11
frenchfish <151521777+frenchfish@users.noreply.github.com>
2026-07-16 22:36:08 +0500

    1

--- src/RomManager/Systems/J2me.cs
+++ src/RomManager/Systems/J2me.cs
@@ -1,6 +1,9 @@
 ﻿namespace RomManager.Systems;
 
-public class J2me
+public class J2me : SystemInfo
 {
-    
+    public override string Name { get; } = "Java 2 Micro Edition";
+    public override string Path { get; } = "j2me";
+    public override string[] Extensions { get; } = { ".jar", ".jad", ".zip" };
+    public override string IconName { get; } = "j2me.png";
 }
